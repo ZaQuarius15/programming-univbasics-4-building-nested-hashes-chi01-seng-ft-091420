76ccf7c3a43b7fb2f640b1b908f3ab23ea388dbe
Zack Carlson <jzcarlson014@gmail.com>
2020-08-21 17:36:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,12 +16,16 @@ def monopoly_with_third_tier
       :four_pieces_owned => 200
     }, 
     :names => {
+      :reading_railroad => {},
+      :pennsylvania_railroad => {},
+      :b_and_o_railroad => {},
+      :shortline_railroad => {}
       
     }
     
   }
     
-  }
+}
 
 end
 
